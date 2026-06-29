f9d1f43d1cef098637ec15004fb82e6cb67c1c68
dhruvs0612 <devsharma009a@gmail.com>
2026-06-29 10:56:17 +0530

    Implement intersection of two arrays in C++
    This program takes two arrays as input and finds their intersection, storing the common elements in a third array.

---
+++ Day-17 assignment /intersection of array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[50], b[50], c[50];
+    int n, m, i, j, k = 0;
+
+    cout << "Enter size of first array: ";
+    cin >> n;
+
+    cout << "Enter elements of first array: ";
+    for (i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    cout << "Enter size of second array: ";
+    cin >> m;
+
+    cout << "Enter elements of second array: ";
+    for (i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    // finding intersection
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            if (a[i] == b[j]) {
+                c[k] = a[i];
+                k++;
+                break;
+            }
+        }
+    }
+
+    cout << "Intersection of arrays is: ";
+    for (i = 0; i < k; i++) {
+        cout << c[i] << " ";
+    }
+
+    return 0;
+}
