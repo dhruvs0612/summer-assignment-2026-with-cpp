84deb841dd17402d080bbd7d50c43ba7a6d9055f
dhruvs0612 <devsharma009a@gmail.com>
2026-06-30 13:07:59 +0530

    Create create mini employee management system.cpp

---
+++ Day-30 assignment/create mini employee management system.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a[20];
+    int b;
+
+    cout << "Mini Employee System" << endl;
+
+    cout << "Enter Name: ";
+    cin >> a;
+
+    cout << "Enter ID: ";
+    cin >> b;
+
+    cout << "\nDetails" << endl;
+    cout << "Name: " << a << endl;
+    cout << "ID: " << b << endl;
+
+    return 0;
+}
